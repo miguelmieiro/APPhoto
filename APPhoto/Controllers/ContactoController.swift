1fb34feffe807181478dae5d5f54de5fc5a92793
Miguel Mieiro <miguelmieiro@miguels-macbook-pro.local>
2018-11-29 11:30:53 +0000

    ponto anterior a introdução de login de utilizador

--- APPhoto/Controllers/ContactoController.swift
+++ APPhoto/Controllers/ContactoController.swift
@@ -15,6 +15,7 @@ class ContactoController: UIViewController, MFMailComposeViewControllerDelegate,
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var telefoneLabel: UILabel!
     @IBOutlet weak var saberMaisLabel: UILabel!
+    @IBOutlet weak var contemLabel: UILabel!
     @IBOutlet weak var aceitoRespostaLabel: UILabel!
     @IBOutlet weak var aceitoPubLabel: UILabel!
     
@@ -33,7 +34,13 @@ class ContactoController: UIViewController, MFMailComposeViewControllerDelegate,
         dismiss(animated: true, completion: nil)
     }
     @IBAction func enviarButton(_ sender: Any) {
-        sendMail()
+        if checkDataEmail() {
+            print("todo ok")
+            sendMail()
+        } else {
+            print("elementos em falta")
+        }
+        
     }
     @IBAction func facebookButton(_ sender: Any) {
     }
@@ -53,6 +60,74 @@ class ContactoController: UIViewController, MFMailComposeViewControllerDelegate,
         mensagemTextView.text = textViewPlaceholder
         mensagemTextView.textColor = UIColor.lightGray
     }
+    
+    
+    
+    //MARK: verificar dados antes de enviar email
+    func checkDataEmail () -> Bool {
+        var check: Int = 0
+        
+        print("Nome - \(String(describing: nomeTextField.text))")
+        print("Email - \(String(describing:emailTextField.text))")
+        print("Telefone - \(String(describing:telefoneTextField.text))")
+        print(umDiaSwitch.state)
+        print(curtasSwitch.state)
+        print(outrosSwitch.state)
+        print("Mensagem - \(String(describing:mensagemTextView.text))")
+        print(respSwitch.state)
+        print(pubSwitch.state)
+        
+        if nomeTextField.text == "" {
+            nomeLabel.textColor = UIColor.red
+            check += 1
+        } else {
+            nomeLabel.textColor = UIColor.black
+        }
+        if emailTextField.text == "" {
+            emailLabel.textColor = UIColor.red
+            check += 1
+        } else {
+            emailLabel.textColor = UIColor.black
+        }
+        if telefoneTextField.text == "" {
+            telefoneLabel.textColor = UIColor.red
+            check += 1
+        }else {
+            telefoneLabel.textColor = UIColor.black
+        }
+        if umDiaSwitch.isOn == false && curtasSwitch.isOn == false && outrosSwitch.isOn == false {
+            saberMaisLabel.textColor = UIColor.red
+            check += 1
+        }else {
+            saberMaisLabel.textColor = UIColor.black
+        }
+        if mensagemTextView.text == textViewPlaceholder {
+            contemLabel.textColor = UIColor.red
+            check += 1
+        }else {
+            contemLabel.textColor = UIColor.black
+        }
+        if respSwitch.isOn == false {
+            aceitoRespostaLabel.textColor = UIColor.red
+            check += 1
+        }else {
+            aceitoRespostaLabel.textColor = UIColor.black
+        }
+        if pubSwitch.isOn == false {
+            aceitoPubLabel.textColor = UIColor.red
+            check += 1
+        }else {
+            aceitoPubLabel.textColor = UIColor.black
+        }
+        
+        if check == 0{
+            return true
+        } else {
+            return false
+        }
+        
+    }
+    
     
     //MARK: Edicao do text view
     func textViewDidBeginEditing(_ textView: UITextView) {
@@ -94,3 +169,4 @@ class ContactoController: UIViewController, MFMailComposeViewControllerDelegate,
     }
     
 }
+
